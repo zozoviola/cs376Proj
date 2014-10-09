c609799b265d55bd62925c28368b96e01b35c49b
Hino <hino17@up.edu>
2014-10-09 00:26:06 +0000

    created hino17 function for hino17 favorite food

---
+++ hino17.c
@@ -0,0 +1,10 @@
+/* This is file hino17.c */
+#include <stdio.h>
+#include "hino17.h"
+
+void hino17() {
+
+  // replace the following print statement with your favorite food.
+  printf("hino17 likes metals with with lots of nickel and copper. \n");
+
+}
---
+++ hino17.h
@@ -0,0 +1,2 @@
+/* This is file hino17.h */
+extern void hino17();
